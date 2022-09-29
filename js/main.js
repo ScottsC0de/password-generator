60b4b87f5afb17270b7b9f659013a5a4bfae7bcf
scottsc0de <scott5902@gmail.com>
2022-09-29 07:35:47 -0400

    added Web API to write password onto page

--- js/main.js
+++ js/main.js
@@ -67,27 +67,10 @@ function generatePassword(userInputLength) {
     var finalPassword = "";
     var finalPasswordValues = upperCaseValues + lowerCaseValues + numberValues + symbolValues;
     var finalPasswordLength = finalPasswordValues.length;
-    for (var = 0; i < userInputLength; i++) {
+    for (var i = 0; i < userInputLength; i++) {
         finalPassword += finalPasswordValues.charAt(Math.floor(Math.random() * finalPasswordLength));
-    };
+    }
+
+    document.getElementById("password").innerHTML = finalPassword;
+
 };
-
-
-
-
-
-
-/* Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); */
