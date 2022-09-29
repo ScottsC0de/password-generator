a699837de24f09ebdf576cb86fc7e9d97f40c858
scottsc0de <scott5902@gmail.com>
2022-09-29 06:49:25 -0400

    added user character length prompt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-// Assignment code here
+// create one function to generate password
+// keep it simple
+// parameter needs to be users length input
+
+var userInputLength = 0;
+
+function generatePassword(userInputLength) {
+    var userInputLength = prompt("How many characters would you like your password to be? (Enter a number between 8 - 128): ")
+    // character length must be 8 - 182
+    if (userInputLength < 8 || userInputLength > 128) {
+        return alert("Please try again and enter a number between 8 and 128")
+    };
+    alert("Please follow the prompts to create your new password");
+    if (userInputLength >= 8 && userInputLength > 128);
+}
 
 
-// Get references to the #generate element
+
+
+
+
+/* Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -14,4 +32,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); */
